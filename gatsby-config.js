46b126093ad4bb127207793f74d1a9dcddc40be0
Daniel Stefan Klose <daniel-stefam.klose@udo.edu>
2021-12-21 13:11:25 +0100

    added fav icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,18 @@ module.exports = {
     `gatsby-plugin-fontawesome-css`,
     "gatsby-transformer-sharp",
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/images/icon.png`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
