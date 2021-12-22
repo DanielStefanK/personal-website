5718ce35bb5e5ae62f4eb2d5481ff32f28a0b73b
Daniel Stefan Klose <daniel-stefam.klose@udo.edu>
2021-12-22 12:42:34 +0100

    fixed manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `daniel-stefan.dev - Dev Portfolio`,
+        short_name: `daniel-stefan.dev`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
