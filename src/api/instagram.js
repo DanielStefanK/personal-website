fa1ca32ae15e5b97c3744df0ce32a9eab3f92c20
Daniel Stefan Klose <daniel-stefam.klose@udo.edu>
2022-04-09 15:42:14 +0200

    fix: fix instagram api

--- src/api/instagram.js
+++ src/api/instagram.js
@@ -1,6 +1,10 @@
-require("node-fetch");
+const fetch = require("node-fetch").default;
 
-const url = `https://www.instagram.com/graphql/query/?query_hash=396983faee97f4b49ccbe105b4daf7a0&variables={"id":"36560492","first":12}`;
+const url =
+  "https://www.instagram.com/graphql/query/?query_hash=396983faee97f4b49ccbe105b4daf7a0&variables=" +
+  encodeURIComponent(
+    '{"id":"36560492","first":12,"after":"QVFES2Y4WTZyTnI5c0Y2YjBTWEhmRXhmN3hnVDBDbjdwX1FWQnQyWHp4bU9hQ04zaFNNQXZRNHRUV0V0dlFQQVRja1V1b3VLSGkwbVVWTVRCSWJiZVcwYQ=="}'
+  );
 
 const cache = {
   lastFetch: 0,
@@ -33,7 +37,7 @@ async function getPosts() {
   return posts;
 }
 
-module.exports = function handler(req, res) {
+module.exports = async function handler(req, res) {
   const posts = await getPosts();
   res.status(200).json(posts);
 };
